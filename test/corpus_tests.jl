22b2f70ab0cc027527165ec6c2f8c65f71aef7a8
Bor Hodošček <bor.hodoscek@gmail.com>
2018-03-29 08:33:09 +0900

    added corpus loading tests

---
+++ test/corpus_tests.jl
@@ -0,0 +1,6 @@
+test_corpus = joinpath(Base.Filesystem.pwd(), "test/test_corpus.txt")
+@testset "Document read tests" begin
+    test_tokens = String["Hello", "there", "This", "is", "a", "sentence"]
+    @test tokens(SimpleDocument(test_corpus)) == test_tokens
+    @test tokens(ComplexityDocument(test_corpus, "", "", "", "", "", 0, "", "", "")) == test_tokens
+end
