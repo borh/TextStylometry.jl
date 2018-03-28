8afe3929b7729956ba435fd56939c6f418402d52
Bor Hodošček <bor.hodoscek@gmail.com>
2018-03-29 08:40:33 +0900

    updated top-level package code

--- src/TextStylometry.jl
+++ src/TextStylometry.jl
@@ -1,5 +1,11 @@
 module TextStylometry
 
-# package code goes here
+include("corpus.jl")
+include("measures.jl")
+include("bootstrap.jl")
+
+export Corpus, GenericDocument, SimpleDocument, ComplexityDocument,
+    tokens, ttr, sttr, mattr, V, P, MWF, guiraud_r, herdan_c,
+    dugast_k, a², dugast_U, LN, brunet_W
 
 end # module
