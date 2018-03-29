5907ae7e1219af30b1d647e9aabd4b1523543a2b
Bor Hodošček <bor.hodoscek@gmail.com>
2018-03-29 11:01:46 +0900

    changed import order; added missing exports

--- src/TextStylometry.jl
+++ src/TextStylometry.jl
@@ -1,12 +1,13 @@
 module TextStylometry
 
 include("corpus.jl")
+include("features.jl")
 include("measures.jl")
 include("bootstrap.jl")
-include("features.jl")
 
 export Corpus, GenericDocument, SimpleDocument, ComplexityDocument,
-    tokens, ttr, sttr, mattr, V, P, MWF, guiraud_r, herdan_c,
+    tokens, equal_windows, bootstrap_window, bootstrap_ci,
+    ttr, sttr, mattr, V, P, MWF, guiraud_r, herdan_c,
     dugast_k, a², dugast_U, LN, brunet_W
 
 end # module
